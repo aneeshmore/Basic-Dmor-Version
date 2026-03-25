9d4c5181372e7613eedc246d1128e739cc80f919
Developer <developer@example.com>
2026-03-25 10:46:19 +0530

    State update successfully

--- server/src/modules/master-products/index.js
+++ server/src/modules/master-products/index.js
@@ -3,3 +3,4 @@ export { masterProductsController } from './controller.js';
 export { MasterProductsService } from './service.js';
 export { MasterProductsRepository } from './repository.js';
 export { MasterProductDTO, ProductDTO } from './dto.js';
+// export
